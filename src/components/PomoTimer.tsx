bbb71b80070a96c4c9dabf5081b0a41af9761783
Ernane Ferreira <ernane.junior25@gmail.com>
2022-05-18 23:20:25 -0300

    WIP: add toast change tabs and confirm change tabs

--- src/components/PomoTimer.tsx
+++ src/components/PomoTimer.tsx
@@ -5,6 +5,7 @@ import { Tabs } from "./Tabs";
 import { notifications } from "../lib/helper";
 
 import clock from '../assets/images/clock.svg';
+import { Toaster } from "react-hot-toast";
 
 export function PomoTimer() {
   const [ currentTab, setCurrentTab ] = useState<number>(0);
@@ -12,14 +13,17 @@ export function PomoTimer() {
   useEffect(() => notifications.requestPermission(), []);
 
   return (
-    <section className={`bg-tab${currentTab == 0 ? "Main" : (currentTab == 1 ? "ShortTime" : "LongTime")} relative overflow-hidden w-screen h-screen flex align-center flex-col justify-between font-mPlus font-w text-md transition-colors duration-1000 select-none`}>
-      <Header title="PomoTimer"/>
-      <main className="w-full h-full flex items-center justify-center z-10">
-        <Tabs currentTab={currentTab} setCurrentTab={setCurrentTab}/>
-      </main>
-      <Footer/>
-      <img src={clock} alt="Relógio analógico" width="512" height="512" className="absolute w-[100vw] h-[100vh] bottom-[-45vh] left-[-40vw] opacity-30 -z-9" />
-      <img src={clock} alt="Relógio analógico" width="512" height="512" className="absolute w-[100vw] h-[100vh] top-[-45vh] right-[-40vw] opacity-30 -z-9" />
-    </section>
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <section className={`bg-tab${currentTab == 0 ? "Main" : (currentTab == 1 ? "ShortTime" : "LongTime")} relative overflow-hidden w-screen h-screen flex align-center flex-col justify-between font-mPlus font-w text-md transition-colors duration-1000 select-none`}>
+        <Header title="PomoTimer"/>
+        <main className="w-full h-full flex items-center justify-center z-10">
+          <Tabs currentTab={currentTab} setCurrentTab={setCurrentTab}/>
+        </main>
+        <Footer/>
+        <img src={clock} alt="Relógio analógico" width="512" height="512" className="absolute w-[100vw] h-[100vh] bottom-[-45vh] left-[-40vw] opacity-30 -z-9" />
+        <img src={clock} alt="Relógio analógico" width="512" height="512" className="absolute w-[100vw] h-[100vh] top-[-45vh] right-[-40vw] opacity-30 -z-9" />
+      </section>
+    </>
     )
   }
--- src/components/Tabs/components/Menu.tsx
+++ src/components/Tabs/components/Menu.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { timerIsChanged, sounds } from "../../../lib/helper";
 import { Time } from "../../../lib/types";
-import { Popup } from "./Popup";
 
 interface MenuProps{
   dataTimer: Time;
@@ -13,32 +13,63 @@ const tabs = [
   {name: "Intervalo Curto", idx: 1},
   {name: "Intervalo Longo", idx: 2}
 ]
-export function Menu({ currentTab, dataTimer, setCurrentTab }: MenuProps){
-  const [popupStatus, setPopupStatus] = useState<boolean>(false);
-  const [tabToChanged, setTabToChanged] = useState<number>(currentTab);
 
+function toastConfirmChangeTab(currentTab:number, tab:number, callback: (tab:number) => void){
+  toast.custom((t) => (
+    <div
+      className={`${
+        t.visible ? 'animate-enter' : 'animate-leave'
+      } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex flex-col items-end ring-1 ring-black ring-opacity-5`}
+    >
+      <div className="flex-1 w-full p-4">
+        <div className="flex items-start">
+          <div className="ml-3 flex-1">
+            <p className="text-sm font-medium text-gray-900">
+              ⚠️ Oopss..!
+            </p>
+            <p className="mt-1 text-sm text-gray-500">
+              Você tem certeza de que deseja reiniciar manualmente o temporizador atual? 🤔
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        <button
+          onClick={() => {callback(tab); toast.dismiss(t.id)}}
+          className={`button-color-tab${currentTab == 0 ? "Main" : (currentTab == 1 ? "ShortTime" : "LongTime")} w-full border border-transparent rounded-none pb-4 pt-2 px-3 flex items-center justify-center text-sm font-medium`}
+        >
+          Confirmar
+        </button>
+        <button
+          onClick={() => toast.dismiss(t.id)}
+          className={`button-color-tab${currentTab == 0 ? "Main" : (currentTab == 1 ? "ShortTime" : "LongTime")} w-full border border-transparent rounded-none pb-4 pt-2 px-3 flex items-center justify-center text-sm font-medium`}
+        >
+          Fechar
+        </button>
+      </div>
+    </div>
+  ));
+}
+
+export function Menu({ currentTab, dataTimer, setCurrentTab }: MenuProps){
   const handleChangeTab = (tab: number) => {
     if(timerIsChanged(dataTimer, currentTab)){
-      setTabToChanged(tab);
-      setPopupStatus(true);
+      toastConfirmChangeTab(currentTab, tab, setCurrentTab);
     }else if(tab !== currentTab){
       setCurrentTab(tab);
     }
   }
   useEffect(() => {
     sounds.tab();
+    toast.success('Tab changed!', {
+      iconTheme: {
+        primary: ["rgb(219,82,77)", "rgb(70,142,145)", "rgb(67,126,168)"][currentTab],
+        secondary: "white"
+      },
+    });
   }, [currentTab]);
   return (
     <>
-    <Popup 
-      title="⚠️ Oopss..!" 
-      status={popupStatus} 
-      setStatus={setPopupStatus} 
-      currentTab={currentTab} 
-      message="Cuidado! ao realizar essa ação o temporizador atual será reiniciado. Deseja continuar? 😰" 
-      callback={() => setCurrentTab(tabToChanged)}
-    />
-
     <nav className="w-full flex items-center justify-between py-2 sm:w-3/4">
       {tabs.map((tab, key)=>(
         <button type="button" 
