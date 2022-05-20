effe82cc5f72220678efc5e598d56727f4073165
Ernane Ferreira <ernane.junior25@gmail.com>
2022-05-19 22:17:13 -0300

    Add icon in header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Alarm } from "phosphor-react";
 import { SetLocaleMenu } from "./Tabs/components/SetLocaleMenu";
 
 interface HeaderProps {
@@ -8,7 +9,9 @@ export function Header({ title }: HeaderProps){
   return (
     <header className="z-20 w-full flex items-center justify-center flex-col  sm:py-5">
       <div className="w-full max-w-5xl border-b border-b-white-200 flex items-center justify-between">
-        <h1 className="block font-semibold text-white text-4xl p-3">{title}</h1>
+        <h1 className="block font-semibold text-white flex items-center gap-2 text-4xl p-3">
+        <Alarm size={32} /> {title}
+        </h1>
         <SetLocaleMenu/>
       </div>
     </header>
